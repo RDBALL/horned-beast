e9768205023ad1132c7a80fde512255b0f50a4f5
Robert Ball <bobball89@gmail.com>
2022-08-15 17:59:37 -0700

    added image import to main.js with placeholder images

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,6 @@
 import React from "react";
+import hornedBeast1 from "./img/placeholder1.jpg"
+import hornedBeast2 from "./img/placeholder2.jpg"
 import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
@@ -6,8 +8,8 @@ class Main extends React.Component {
     return (
       <main>
       <div className='hornedBeastMain'>
-        <HornedBeast title='Horned Beast 1' imageURL='#' description='Description of Horned Beast 1'/>
-        <HornedBeast title='Horned Beast 2' imageURL='#' description='Description of Horned Beast 2'/>
+        <HornedBeast title='Horned Beast 1' imageURL={hornedBeast1} description='Description of Horned Beast 1'/>
+        <HornedBeast title='Horned Beast 2' imageURL={hornedBeast2}  description='Description of Horned Beast 2'/>
       </div> 
       </main>
       )
