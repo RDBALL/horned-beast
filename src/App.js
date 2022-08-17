35a1a3862fe9f8dfdbaa895d03bd2855ff27b5d1
Robert Ball <bobball89@gmail.com>
2022-08-16 17:17:17 -0700

    updated app.js, footer.js, hornedbeast.js to import styles from app, footer, header css

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
 import './App.css';
+import './Header.css';
+import './Footer.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
         <Header />
-        <div className="beastLayout">
+        <div className="cardLayout">
         <Main />
         </div>
         <Footer />
